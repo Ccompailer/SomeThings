cad87fb1becb1befff53c3775c5b4e7deaa89c85
Anton Balabanov <balabanov-03@list.ru>
2024-01-13 20:50:18 +0300

    Использование псевдонима типа для указателя на функцию

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,21 @@
 
 template<typename From>
 using ShortCaster = NarrowCaster<short, From>;
+float Test(int, float);
 
 int main() {
     int five = 5;
+
+    using TestPointer = float (*) (int, float);
+
+    TestPointer pointer = Test;
     ShortCaster<int> caster;
     auto testCast = caster.Cast(five);
-    std::cout << typeid(testCast).name();
+    std::cout << typeid(testCast).name() << "\n";
+    std::cout << pointer(5, 5.1f);
     return 0;
+}
+
+float Test(int a, float b) {
+    return static_cast<float>(a) + b;
 }
