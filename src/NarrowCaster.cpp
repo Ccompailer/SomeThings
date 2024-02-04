067f12681b2e1f10a054a8a6e2094fde8fc69cdf
Anton Balabanov <balabanov-03@list.ru>
2024-02-04 20:54:12 +0300

    A little bit changes

--- src/NarrowCaster.cpp
+++ src/NarrowCaster.cpp
@@ -4,6 +4,7 @@ template<typename To, typename From>
 struct NarrowCaster {
     To Cast(From obj) {
         const auto converted = static_cast<To>(obj);
+
         const auto backwards = static_cast<From>(converted);
 
         if (obj != backwards)
